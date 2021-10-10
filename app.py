cfc7054fc7e0ca98baaaf67136c34698fa2d3793
Michael Pinna <mikepinna3@gmail.com>
2021-10-10 18:37:33 -0400

    time_chart

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-# from datetime import time
+from datetime import time
 
 app = Flask(__name__)
 
@@ -37,4 +37,36 @@ def line_chart():
         legend_1=legend,
         values_1=temperatures,
         labels=times
+    )
+
+
+@app.route('/time_chart')
+def time_chart():
+    legend = 'Temperatures'
+    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
+                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
+                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
+    times = [time(hour=11, minute=14, second=15),
+             time(hour=11, minute=14, second=30),
+             time(hour=11, minute=14, second=45),
+             time(hour=11, minute=15, second=00),
+             time(hour=11, minute=15, second=15),
+             time(hour=11, minute=15, second=30),
+             time(hour=11, minute=15, second=45),
+             time(hour=11, minute=16, second=00),
+             time(hour=11, minute=16, second=15),
+             time(hour=11, minute=16, second=30),
+             time(hour=11, minute=16, second=45),
+             time(hour=11, minute=17, second=00),
+             time(hour=11, minute=17, second=15),
+             time(hour=11, minute=17, second=30),
+             time(hour=11, minute=17, second=45),
+             time(hour=11, minute=18, second=00),
+             time(hour=11, minute=18, second=15),
+             time(hour=11, minute=18, second=30)]
+    return render_template(
+        'time_chart.html',
+        values=temperatures,
+        labels=times,
+        legend=legend
     )
